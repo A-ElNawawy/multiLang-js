48cd143659942284fff3d61fad3231e588f20268
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2021-01-30 17:02:06 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// define data variable
+let data;
+
+// set default language
+function setDefaultLang(){
+  (localStorage.getItem('language') == null) ? setLanguage('en') : false;
+}
+
+// get data according to the language setting
+function getData(){
+  $.ajax({
+    url: '/language/' + localStorage.getItem('language') + '.json',
+    dataType: 'json',
+    async: false,
+    success: function (res /* response we get */) { data = res }
+  });
+}
+
+// display data we get
+function displayData() {
+  $(document).ready(function(){
+    $('#name').text(data.name);
+    $('#age').text(data.age);
+    $('#date').text(data.date);
+  });
+}
+
+// set language
+function setLanguage(lang) {
+  localStorage.setItem('language', lang);
+}
+
+// change language
+function changeLang(lang){
+  setLanguage(lang);
+  getData();
+  displayData();
+}
+
+setDefaultLang();
+getData();
+displayData();
---
+++ readme.md
@@ -0,0 +1,11 @@
+# MultiLang-js
+It is a small tutorial discussing how to make multiple languages app
+
+# Source
+Youtube Video
+
+# Libraries and Dependencies
+jQuery
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)

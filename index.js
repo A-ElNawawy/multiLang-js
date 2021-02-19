05259b5ed585e351742f6246249c0e12ce14c26a
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2021-02-19 22:23:08 +0200

    exchange jquery method by native one

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ function setDefaultLang(){
 
 // get data according to the language setting
 function getData(){
-  $.ajax({
-    url: '/language/' + localStorage.getItem('language') + '.json',
-    dataType: 'json',
-    async: false,
-    success: function (res /* response we get */) { data = res }
-  });
+  //$.ajax({
+  //  url: '/language/' + localStorage.getItem('language') + '.json',
+  //  dataType: 'json',
+  //  async: false,
+  //  success: function (res /* response we get */) { data = res }
+  //});
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+      console.log(data);
+    }
+  };
+  xhttp.open("GET", '/language/' + localStorage.getItem('language') + '.json', false);
+  xhttp.send();
 }
 
 // display data we get
